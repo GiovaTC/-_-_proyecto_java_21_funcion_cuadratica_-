633916713c4778c67b8f690e51cd48afd28c696b
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-16 10:43:27 -0500

    commit - Main .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("====================================");
+        System.out.println(" CALCULADORA DE ECUACION CUADRATICA ");
+        System.out.println("====================================");
+
+        System.out.print("Ingrese el valor de a: ");
+        double a = teclado.nextDouble();
+
+        System.out.print("Ingrese el valor de b: ");
+        double b = teclado.nextDouble();
+
+        System.out.print("Ingrese el valor de c: ");
+        double c = teclado.nextDouble();
+
+        if ( a == 0) {
+            System.out.println();
+            System.out.println("El valor de 'a' no puede ser cero.");
+            System.out.println("No es una ecuacion cuadratica.");
+            return;
         }
+
+        double discriminante = (b * b) - (4 * a * c);
+
+        System.out.println();
+        System.out.println("Discriminante = " + discriminante);
+
+        if (discriminante > 0) {
+
+            double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+
+            System.out.println();
+            System.out.println("La ecuacion tiene dos soluciones reales:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        } else if (discriminante == 0) {
+
+            double x = (-b) / (2 * a);
+            System.out.println();
+            System.out.println("La ecuacion tiene una unica solucion real:");
+            System.out.println("x = " + x);
+
+        } else {
+
+            double parteReal = (-b) / (2 * a);
+            double parteImaginaria = Math.sqrt(-discriminante) / (2 * a);
+
+            System.out.println();
+            System.out.println("La ecuacion tiene dos soluciones complejas:");
+            System.out.println("x1 = " + parteReal + " + " + parteImaginaria + "i");
+            System.out.println("x2 = " + parteReal + " - " + parteImaginaria + "i");
+        }
+
+        teclado.close();
     }
 }
